413506944ed76559da710147ef87f5fbd8909f5d
Jesher Rodrigues <jeshernash@gmail.com>
2020-11-08 18:35:00 -0300

    new version terraform

--- infra/vars.tf
+++ infra/vars.tf
@@ -1,8 +1,8 @@
-variable "AWS_ACCESS_KEY_ID" {
-}
+# variable "AWS_ACCESS_KEY_ID" {
+# }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-}
+# variable "AWS_SECRET_ACCESS_KEY" {
+# }
 
 variable "AWS_REGION" {
   default = "us-east-1"
