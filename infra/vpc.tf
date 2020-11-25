756365465e4de1886184d026e2f5d8c0fb94e31f
Jesher Rodrigues <jeshernash@gmail.com>
2020-11-25 19:43:59 -0300

    test jq for alb2

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "${var.project}-${var.environment}-ecs-cluster"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["us-east-1a", "us-east-1c",]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
