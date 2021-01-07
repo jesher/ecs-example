e8b8faff63fe69d32bb87c8e1c86a627a6a961cb
Jesher Rodrigues <jesher.rodrigues@getrak.com.br>
2021-01-07 12:19:04 -0300

    add test stress

--- api-test/src/server.ts
+++ api-test/src/server.ts
@@ -1,10 +1,27 @@
 require('dotenv').config()
 
-import express, { response } from 'express';
+import express from 'express';
+import {exec} from 'child_process';
 
 const app = express()
 
 app.get('/v1/healthcheck', (request, response) => {
+    console.log(request.header('x-forwarded-for') || request.connection.remoteAddress)
+    return response.json({message: "test api ok!", tag_image: process.env.HASH_COMMIT})
+})
+
+app.get('/v1/test', (request, response) => {
+    exec('stress --cpu 8 --io 4 --vm 4 --vm-bytes 1024M --timeout 10s', (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          return;
+        }
+      
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+
     return response.json({message: "test api ok!", tag_image: process.env.HASH_COMMIT})
 })
 
